62636bf1c0aa3ccfb97216522a1a5dc8dae435df
Gerald Bryant <hakeem1106@gmail.com>
2019-04-30 20:40:28 +0000

    centered finally. Now to add more routes and pages

--- src/index.js
+++ src/index.js
@@ -1,32 +1,3 @@
-/*const segment = document.getElementById('root'),
-      form = document.createElement("form"),
-      userName = document.createElement("input"),
-      passWord = document.createElement("input"),
-      subBut = document.createElement("button");
-
-segment.appendChild(form).setAttribute("method", 'POST')
-
-userName.setAttribute('name', "username");
-passWord.setAttribute('name', "password");
-
-
-form.setAttribute("action", "/index")
-form.appendChild(userName);
-form.appendChild(passWord);
-form.appendChild(subBut);
-
-
-
-
-
-userName.setAttribute("placeholder", "Username");
-passWord.setAttribute("placeholder", "Password");
-subBut.setAttribute("type","submit")
-subBut.innerHTML = "Submit";
-
-
-*/
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './modules/login'
--- src/modules/login.js
+++ src/modules/login.js
@@ -6,13 +6,13 @@ export default class Login extends React.Component{
     render(){
         return(
             <div>
-            <div>
+            <div id="formy" className="position-absolute">
             <form method="POST" action="/index">
-            <input type='text' placeholder="Username" name="username"></input>
+            <input type='text'className="form-control" placeholder="Username" name="username"></input>
             <br></br>
-            <input type='text' placeholder="Password" name="password"></input>
+            <input type="password" className="form-control" placeholder="Password" name="password"></input>
             <br></br>
-            <button type="submit">Submit</button>
+            <button type="submit" className="btn btn-primary">Submit</button>
             </form>
             </div>
             </div>
